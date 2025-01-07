0c685722f545f66f0bb72351b4b63c6e1ff01bb5
Brooks Cunningham <bcunningham@fastly.com>
2025-01-07 09:01:41 -0600

    Updates

--- ngwaf-compute-integration/README.md
+++ ngwaf-compute-integration/README.md
@@ -4,3 +4,20 @@ Pass requests to Fastly's Next-Gen Web Application Firewall (Next-Gen WAF) from 
 
 This implementation is inspired by the following documentation.
 https://www.fastly.com/documentation/solutions/tutorials/next-gen-waf-compute/
+
+Do not forget to link your NGWAF edge deployment instance to the Compute service.
+```
+curl -X PUT "https://dashboard.signalsciences.net/api/v0/corps/${corpName}/sites/${siteName}/edgeDeployment" \
+-H "x-api-user:${SIGSCI_EMAIL}" \
+-H "x-api-token:${SIGSCI_TOKEN}" \
+-H "Fastly-Key: ${FASTLY_KEY}" \
+-H "Content-Type: application/json" \
+-d '{"authorizedServices": [ "${fastlySID}" ] }'
+```
+
+Check the linked service with the following command.
+```
+curl -H "x-api-user:${SIGSCI_EMAIL}" -H "x-api-token:${SIGSCI_TOKEN}" \
+-H "Content-Type: application/json" \
+"https://dashboard.signalsciences.net/api/v0/corps/${corpName}/sites/${siteName}/edgeDeployment"
+```
--- ngwaf-compute-integration/src/main.rs
+++ ngwaf-compute-integration/src/main.rs
@@ -1,121 +1,51 @@
-use fastly::security::{inspect, InspectConfig, InspectError, InspectResponse};
+use fastly::security::{inspect, InspectConfig, InspectError, InspectResponse, InspectVerdict};
 use fastly::handle::BodyHandle;
-use fastly::http::StatusCode;
+use fastly::http::{HeaderName, HeaderValue, StatusCode};
 use fastly::{Request, Response};
 
 const HTTPME_BACKEND: &str = "HTTPME";
 
 #[fastly::main]
-fn main(mut req: Request) -> Result<Response, fastly::Error> {
-    // Log service version
-    println!(
-        "FASTLY_SERVICE_VERSION: {}",
-        std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_else(|_| String::new())
-    );
+fn main(req: Request) -> Result<Response, fastly::Error> {
+    let ngwaf_config = fastly::config_store::ConfigStore::open("ngwaf");
+    let corp_name = ngwaf_config
+        .get("corp")
+        .expect("no `corp` present in config");
+    let ws_name = ngwaf_config
+        .get("workspace")
+        .expect("no `workspace` present in config");
 
-    // Do not cache requests
-    req.set_pass(true);
+    let (mut req_handle, req_body) = req.into_handles();
+    let req_body = req_body.unwrap_or_else(|| BodyHandle::new());
+    let config = InspectConfig::new(&req_handle, &req_body)
+        .corp(corp_name)
+        .workspace(ws_name);
 
-    // Returns req to allow for setting request headers based on the WAF inspection.
-    let (mut req, waf_inspection_result) = do_waf_inspect(req);
+    match inspect(config) {
+        Ok(resp) => match resp.verdict() {
+            InspectVerdict::Block => Ok(Response::from_status(StatusCode::NOT_ACCEPTABLE)),
+            InspectVerdict::Allow => {
 
-    // if the waf inspection has an error, then return the error.
-    if waf_inspection_result.get_status() != 200 {
-        return Ok(waf_inspection_result);
-    }
+                let waf_inspection_header_val = format_waf_inspection_header(resp);    
+                req_handle.insert_header(&HeaderName::from_static("waf-inspect-data"), &HeaderValue::from_str(&waf_inspection_header_val)?);
+                req_handle.insert_header(&HeaderName::from_static("host"), &HeaderValue::from_static("http.edgecompute.app"));
 
-    req.set_header("host", "http.edgecompute.app");
 
-    // Send request to the backend
-    let resp: Response = req.send(HTTPME_BACKEND)?;
-
-    // Return the response back to the client
-    return Ok(resp);
-}
-
-fn do_waf_inspect(mut req: Request) -> (Request, Response) {
-    // if bypass-waf is present, then do not send the request to the WAF for processing
-    match req.get_header_str("bypass-waf") {
-        Some(_) => {
-            println!("bypassing waf");
-            return (
-                req,
-                Response::from_status(StatusCode::OK).with_set_header(
-                    "x-version",
-                    std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_else(|_| String::new()),
-                ),
-            );
-        }
-        _ => {
-            let ngwaf_config = fastly::config_store::ConfigStore::open("ngwaf");
-            let corp_name = ngwaf_config
-                .get("corp")
-                .expect("no `corp` present in config");
-            let site_name = ngwaf_config
-                .get("site")
-                .expect("no `site` present in config");
-
-            // clone the request and send the cloned request to the waf inspection.
-            let inspection_req = req.clone_with_body();
-            let (reqhandle, bodyhandle) = inspection_req.into_handles();
-            let bodyhandle = bodyhandle.unwrap_or_else(|| BodyHandle::new());
-
-            let inspectconf: InspectConfig<'_> = InspectConfig::new(&reqhandle, &bodyhandle)
-                .corp(&corp_name)
-                .workspace(&site_name);
-            let waf_result: Result<InspectResponse, InspectError> = inspect(inspectconf);
-
-            match waf_result {
-                Ok(inspect_resp) => {
-                    // Handling WAF result
-                    println!(
-                    "waf_status_code: {}\nwaf_tags: {:?}\nwaf_decision_ms: {:?}\nwaf_verdict: {:?}",
-                    inspect_resp.status(),
-                    inspect_resp.tags(),
-                    inspect_resp.decision_ms(),
-                    inspect_resp.verdict(),
-                    );
-                    let waf_inspection_header_val = format_waf_inspection_header(inspect_resp);
-                    
-                    req.set_header("waf-inspect-data", waf_inspection_header_val);
-                }
-                Err(y) => match y {
-                    InspectError::InvalidConfig => {
-                        println!("NGWAF failed because of invalid configuration");
-                        return (
-                            req,
-                            Response::from_status(StatusCode::SERVICE_UNAVAILABLE).with_set_header(
-                                "x-version",
-                                std::env::var("FASTLY_SERVICE_VERSION")
-                                    .unwrap_or_else(|_| String::new()),
-                            ),
-                        );
-                    }
-                    InspectError::RequestError(f) => {
-                        println!(
-                    "Failed to send an inspection request to the NGWAF FastlyStatusCode: {}",
-                    f.code
-                );
-                        return (
-                            req,
-                            Response::from_status(StatusCode::SERVICE_UNAVAILABLE).with_set_header(
-                                "x-version",
-                                std::env::var("FASTLY_SERVICE_VERSION")
-                                    .unwrap_or_else(|_| String::new()),
-                            ),
-                        );
-                    }
-                    _ => println!("Catch-all waf_result"),
-                },
-            };
-
-            return (
-                req,
-                Response::from_status(StatusCode::OK).with_set_header(
-                    "x-version",
-                    std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_else(|_| String::new()),
-                ),
-            );
+                // Send request to the backend
+                let resp = Request::from_handles(req_handle, Some(req_body)).send(HTTPME_BACKEND)?;
+            
+                // Return the response back to the client
+                return Ok(resp);
+            },
+            InspectVerdict::Unauthorized => {
+                panic!("The service is not authorized to inspect the request")
+            },
+            _ => Ok(Response::from_status(StatusCode::INTERNAL_SERVER_ERROR)
+                .with_body("Unable to inspect request")),
+        },
+        Err(err) => {
+            let msg = format!("Invalid request: {err:?}");
+            Ok(Response::from_status(StatusCode::BAD_REQUEST).with_body(msg))
         }
     }
 }
