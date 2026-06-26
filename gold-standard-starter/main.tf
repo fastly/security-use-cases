38d50a65645c253efe7ae3c3d78b2544879f42ea
Sascha Nowak <github@saschanowak.me>
2026-06-26 10:31:33 +0200

    feat: convert ngwaf gold-standard-starter to fastly terraform provider

--- gold-standard-starter/main.tf
+++ gold-standard-starter/main.tf
@@ -1,25 +1,31 @@
 # Try to follow recommended practices here, https://developer.hashicorp.com/terraform/cloud-docs/recommended-practices
 
 #### Supply NGWAF API authentication - Start
-# environment variables must be available using "TF_VAR_*" in your terminal. 
-# For example, `echo $TF_VAR_NGWAF_CORP` should return your intended corp.
-provider "sigsci" {
-  corp       = var.NGWAF_CORP
-  email      = var.NGWAF_EMAIL
-  auth_token = var.NGWAF_TOKEN
+# environment variables must be available using "TF_VAR_*" in your terminal.
+# For example, `echo $TF_VAR_NGWAF_WORKSPACE` should return your intended corp.
+provider "fastly" {
+  # Authenticates via FASTLY_API_KEY environment variable.
+  # The NGWAF resources use the same API key as all other Fastly resources.
 }
 #### Supply NGWAF API authentication - End
 
-# resource "sigsci_site" "tenant_site" {
-#   short_name = var.NGWAF_SITE
-#   display_name = var.NGWAF_SITE
-#   block_duration_seconds = 86400
-#   agent_anon_mode = ""
-#   agent_level = "log"
-# }
+#### NGWAF Workspace - Start
+resource "fastly_ngwaf_workspace" "tenant_workspace" {
+  name        = var.NGWAF_WORKSPACE
+  description = "Fastly Gold Standard Starter"
+  mode        = "log"
+
+  attack_signal_thresholds {
+    one_minute  = 50
+    ten_minutes = 350
+    one_hour    = 1800
+    immediate   = false
+  }
+}
+#### NGWAF Workspace - End
 
 #### Block Any Attack Signal from Attack Sources - Start
-resource "sigsci_corp_list" "system-attack-signals-list" {
+resource "fastly_ngwaf_account_list" "system-attack-signals-list" {
   name = "system-attack-signals"
   type = "signal"
   entries = [
@@ -33,7 +39,7 @@ resource "sigsci_corp_list" "system-attack-signals-list" {
   ]
 }
 
-resource "sigsci_corp_list" "attack-sources-signals-list" {
+resource "fastly_ngwaf_account_list" "attack-sources-signals-list" {
   name = "attack-sources-signals"
   type = "signal"
   entries = [
@@ -43,52 +49,48 @@ resource "sigsci_corp_list" "attack-sources-signals-list" {
   ]
 }
 
-resource "sigsci_corp_signal_tag" "malicious-attacker-signal" {
-  short_name  = "malicious-attacker"
+resource "fastly_ngwaf_account_signal" "malicious-attacker-signal" {
+  name        = "malicious-attacker"
   description = "Identification of attacks from attacking IPs"
+  applies_to  = ["*"]
 }
 
-resource "sigsci_corp_rule" "malicious-attacker-rule" {
-  site_short_names = []
-  type             = "request"
-  corp_scope       = "global"
-  group_operator   = "all"
-  enabled          = true
-  reason           = "Detect attacks from known attacking IPs"
-  expiration       = ""
+resource "fastly_ngwaf_account_rule" "malicious-attacker-rule" {
+  applies_to      = ["*"]
+  type            = "request"
+  description     = "Detect attacks from known attacking IPs"
+  enabled         = true
+  group_operator  = "all"
+  request_logging = "sampled"
 
-
-  conditions {
-    type           = "multival"
+  multival_condition {
     field          = "signal"
     group_operator = "all"
     operator       = "exists"
 
-    conditions {
-      type     = "single"
-      field    = "signalType"
-      operator = "inList"
-      value    = sigsci_corp_list.system-attack-signals-list.id
+    condition {
+      field    = "signal_id"
+      operator = "in_list"
+      value    = "corp.${fastly_ngwaf_account_list.system-attack-signals-list.name}"
     }
-    conditions {
-      type     = "single"
-      field    = "signalType"
-      operator = "inList"
-      value    = sigsci_corp_list.attack-sources-signals-list.id
+    condition {
+      field    = "signal_id"
+      operator = "in_list"
+      value    = "corp.${fastly_ngwaf_account_list.attack-sources-signals-list.name}"
     }
   }
   # Easily go into blocking by uncommenting the following action
-  # actions {
+  # action {
   #   type = "block"
   # }
-  actions {
-    type   = "addSignal"
-    signal = sigsci_corp_signal_tag.malicious-attacker-signal.id
+  action {
+    type   = "add_signal"
+    signal = "corp.${fastly_ngwaf_account_signal.malicious-attacker-signal.name}"
   }
 
   depends_on = [
-    sigsci_corp_list.system-attack-signals-list,
-    sigsci_corp_list.attack-sources-signals-list,
+    fastly_ngwaf_account_list.system-attack-signals-list,
+    fastly_ngwaf_account_list.attack-sources-signals-list,
   ]
 }
 #### Block Any Attack Signal from Attack Sources - End
@@ -97,12 +99,13 @@ resource "sigsci_corp_rule" "malicious-attacker-rule" {
 
 #### Block Requests from Countries that are not revenue generating - Start
 # Also consider, https://home.treasury.gov/policy-issues/office-of-foreign-assets-control-sanctions-programs-and-information
-resource "sigsci_corp_signal_tag" "blocked-countries-corp-signal" {
-  short_name  = "blocked-countries"
+resource "fastly_ngwaf_account_signal" "blocked-countries-corp-signal" {
+  name        = "blocked-countries"
   description = "Block countries that are not revenue generating"
+  applies_to  = ["*"]
 }
 
-resource "sigsci_corp_list" "blocked-countries-corp-list" {
+resource "fastly_ngwaf_account_list" "blocked-countries-corp-list" {
   name = "blocked-countries"
   type = "country"
   entries = [
@@ -111,35 +114,33 @@ resource "sigsci_corp_list" "blocked-countries-corp-list" {
   description = "Block countries that are not revenue generating. KP is North Korea."
 }
 
-resource "sigsci_corp_rule" "blocked-countries-corp-rule" {
-  site_short_names = []
-  type             = "request"
-  corp_scope       = "global"
-  enabled          = true
-  group_operator   = "all"
-  reason           = "Country Blocking Rule"
-  expiration       = ""
+resource "fastly_ngwaf_account_rule" "blocked-countries-corp-rule" {
+  applies_to      = ["*"]
+  type            = "request"
+  description     = "Country Blocking Rule"
+  enabled         = true
+  group_operator  = "all"
+  request_logging = "sampled"
 
-  conditions {
-    type     = "single"
+  condition {
     field    = "country"
-    operator = "inList"
-    value    = sigsci_corp_list.blocked-countries-corp-list.id
+    operator = "in_list"
+    value    = "corp.${fastly_ngwaf_account_list.blocked-countries-corp-list.name}"
   }
 
   # Easily go into blocking by uncommenting the following action
-  # actions {
+  # action {
   #   type = "block"
   # }
 
-  actions {
-    type   = "addSignal"
-    signal = sigsci_corp_signal_tag.blocked-countries-corp-signal.id
+  action {
+    type   = "add_signal"
+    signal = "corp.${fastly_ngwaf_account_signal.blocked-countries-corp-signal.name}"
   }
 
   depends_on = [
-    sigsci_corp_list.blocked-countries-corp-list,
-    sigsci_corp_signal_tag.blocked-countries-corp-signal,
+    fastly_ngwaf_account_list.blocked-countries-corp-list,
+    fastly_ngwaf_account_signal.blocked-countries-corp-signal,
   ]
 }
 #### Block Requests from Countries that are not revenue generating - End
@@ -147,102 +148,53 @@ resource "sigsci_corp_rule" "blocked-countries-corp-rule" {
 
 
 #### Lower Attack Thresholds - Start
-resource "sigsci_corp_signal_tag" "system-attack-signal" {
-  short_name  = "system-attack"
+resource "fastly_ngwaf_account_signal" "system-attack-signal" {
+  name        = "system-attack"
   description = "Tagging requests with that match any attack"
+  applies_to  = ["*"]
 }
 
-resource "sigsci_corp_rule" "system-attack-rule" {
-  site_short_names = []
-  type             = "request"
-  corp_scope       = "global"
-  group_operator   = "all"
-  enabled          = true
-  reason           = "Add a signal for any attack"
-  expiration       = ""
+resource "fastly_ngwaf_account_rule" "system-attack-rule" {
+  applies_to      = ["*"]
+  type            = "request"
+  description     = "Add a signal for any attack"
+  enabled         = true
+  group_operator  = "all"
+  request_logging = "sampled"
 
-  conditions {
-    type           = "multival"
+  multival_condition {
     field          = "signal"
     group_operator = "all"
     operator       = "exists"
 
-    conditions {
-      type     = "single"
-      field    = "signalType"
-      operator = "inList"
-      value    = sigsci_corp_list.system-attack-signals-list.id
+    condition {
+      field    = "signal_id"
+      operator = "in_list"
+      value    = "corp.${fastly_ngwaf_account_list.system-attack-signals-list.name}"
     }
   }
   #### Easily go into blocking by uncommenting the following action
-  # actions {
+  # action {
   #   type = "block"
   # }
-  actions {
-    type   = "addSignal"
-    signal = sigsci_corp_signal_tag.system-attack-signal.id
+  action {
+    type   = "add_signal"
+    signal = "corp.${fastly_ngwaf_account_signal.system-attack-signal.name}"
   }
   depends_on = [
-    sigsci_corp_list.system-attack-signals-list,
-    sigsci_corp_signal_tag.system-attack-signal,
+    fastly_ngwaf_account_list.system-attack-signals-list,
+    fastly_ngwaf_account_signal.system-attack-signal,
   ]
 }
 
-# Lower the thresholds for any system attack - Start
-# Docs. https://registry.terraform.io/providers/signalsciences/sigsci/latest/docs/resources/site_alert
-resource "sigsci_site_alert" "any-attack-site-alert-1min" {
-  site_short_name    = var.NGWAF_SITE
-  tag_name           = sigsci_corp_signal_tag.system-attack-signal.id
-  long_name          = "Any system attack alert 1 min"
-  interval           = 1
-  threshold          = 50
-  enabled            = true
-  action             = "info"
-  skip_notifications = true
-
-  depends_on = [
-    sigsci_corp_signal_tag.system-attack-signal,
-  ]
-}
-
-resource "sigsci_site_alert" "any-attack-site-alert-10min" {
-  site_short_name    = var.NGWAF_SITE
-  tag_name           = sigsci_corp_signal_tag.system-attack-signal.id
-  long_name          = "Any system attack alert 10 min"
-  interval           = 10
-  threshold          = 350
-  enabled            = true
-  action             = "info"
-  skip_notifications = true
-
-  depends_on = [
-    sigsci_corp_signal_tag.system-attack-signal
-  ]
-}
-
-resource "sigsci_site_alert" "any-attack-site-alert-60min" {
-  site_short_name    = var.NGWAF_SITE
-  tag_name           = sigsci_corp_signal_tag.system-attack-signal.id
-  long_name          = "Any system attack alert 60 min"
-  interval           = 60
-  threshold          = 1800
-  enabled            = true
-  action             = "info"
-  skip_notifications = true
-
-  depends_on = [
-    sigsci_corp_signal_tag.system-attack-signal
-  ]
-}
-#### Lower Attack Thresholds - End
-
-#### Anomoly Signals - Start
-resource "sigsci_corp_signal_tag" "anomaly-attack-signal" {
-  short_name  = "anomaly-attack"
+#### Anomaly Signals - Start
+resource "fastly_ngwaf_account_signal" "anomaly-attack-signal" {
+  name        = "anomaly-attack"
   description = "Identification of attacks from Anomaly traffic"
+  applies_to  = ["*"]
 }
 
-resource "sigsci_corp_list" "anomaly-attack-signals-list" {
+resource "fastly_ngwaf_account_list" "anomaly-attack-signals-list" {
   name = "anomaly-attack-signals"
   type = "signal"
   entries = [
@@ -257,94 +209,87 @@ resource "sigsci_corp_list" "anomaly-attack-signals-list" {
   ]
 }
 
-resource "sigsci_corp_rule" "anomaly-attack-corp-rule" {
-  site_short_names = []
-  type             = "request"
-  corp_scope       = "global"
-  group_operator   = "all"
-  enabled          = true
-  reason           = "Identify attacks from Anomaly Traffic"
-  expiration       = ""
-  conditions {
-    type           = "multival"
+resource "fastly_ngwaf_account_rule" "anomaly-attack-corp-rule" {
+  applies_to      = ["*"]
+  type            = "request"
+  description     = "Identify attacks from Anomaly Traffic"
+  enabled         = true
+  group_operator  = "all"
+  request_logging = "sampled"
+
+  multival_condition {
     field          = "signal"
     group_operator = "all"
     operator       = "exists"
 
-    conditions {
-      type     = "single"
-      field    = "signalType"
-      operator = "inList"
-      value    = sigsci_corp_list.anomaly-attack-signals-list.id
+    condition {
+      field    = "signal_id"
+      operator = "in_list"
+      value    = "corp.${fastly_ngwaf_account_list.anomaly-attack-signals-list.name}"
     }
   }
-  actions {
-    type   = "addSignal"
-    signal = sigsci_corp_signal_tag.anomaly-attack-signal.id
+  action {
+    type   = "add_signal"
+    signal = "corp.${fastly_ngwaf_account_signal.anomaly-attack-signal.name}"
   }
   #### Easily go into blocking by uncommenting the following action
-  # actions {
+  # action {
   #   type = "block"
   # }
   depends_on = [
-    sigsci_corp_list.anomaly-attack-signals-list,
-    sigsci_corp_signal_tag.anomaly-attack-signal,
+    fastly_ngwaf_account_list.anomaly-attack-signals-list,
+    fastly_ngwaf_account_signal.anomaly-attack-signal,
   ]
 }
-#### Anomoly Signals - End
+#### Anomaly Signals - End
 
 #### Rate Limiting Enumeration Attempts - Start
-resource "sigsci_site_signal_tag" "bad-response-signal" {
-  site_short_name = var.NGWAF_SITE
-  name            = "bad-response"
-  description     = "Identification of attacks from malicious IPs"
-
+resource "fastly_ngwaf_workspace_signal" "bad-response-signal" {
+  workspace_id = fastly_ngwaf_workspace.tenant_workspace.id
+  name         = "bad-response"
+  description  = "Identification of attacks from malicious IPs"
 }
 
-resource "sigsci_site_rule" "enumeration-attack-rule" {
-  site_short_name = var.NGWAF_SITE
-  type            = "rateLimit"
-  group_operator  = "any"
-  enabled         = true
-  reason          = "Blocking IPs that have too many bad responses. Likely an enumeration attack."
-  expiration      = ""
+resource "fastly_ngwaf_workspace_rule" "enumeration-attack-rule" {
+  workspace_id   = fastly_ngwaf_workspace.tenant_workspace.id
+  type           = "rate_limit"
+  group_operator = "any"
+  enabled        = true
+  description    = "Blocking IPs that have too many bad responses. Likely an enumeration attack."
 
-  conditions {
-    type     = "single"
-    field    = "responseCode"
+  condition {
+    field    = "response_code"
     operator = "like"
     value    = "4[0-9][0-9]"
   }
-  conditions {
-    type     = "single"
-    field    = "responseCode"
+  condition {
+    field    = "response_code"
     operator = "like"
     value    = "5[0-9][0-9]"
   }
-  # actions {
-  #   type          = "blockSignal"
+  # action {
+  #   type          = "block_signal"
   #   signal        = "ALL-REQUESTS"
   #   response_code = 406
   # }
 
-  actions {
-    type   = "logRequest"
-    signal = sigsci_site_signal_tag.bad-response-signal.id
+  action {
+    type   = "log_request"
+    signal = "site.${fastly_ngwaf_workspace_signal.bad-response-signal.name}"
   }
 
   rate_limit {
     threshold = 10
-    interval  = 1
+    interval  = 60
     duration  = 600
+    signal    = "site.${fastly_ngwaf_workspace_signal.bad-response-signal.name}"
     client_identifiers {
-      type = "ip" # Defaults to IP
+      type = "ip"
     }
   }
 
-  signal = sigsci_site_signal_tag.bad-response-signal.id
-
   depends_on = [
-    sigsci_site_signal_tag.bad-response-signal,
+    fastly_ngwaf_workspace_signal.bad-response-signal,
   ]
 }
 
@@ -354,8 +299,8 @@ resource "sigsci_site_rule" "enumeration-attack-rule" {
 output "live_waf_love_output" {
   value = <<tfmultiline
 
-  #### Click the URL to go to the Fastly NGWAF service ####
-  https://dashboard.signalsciences.net/corps/${var.NGWAF_CORP}/sites/${var.NGWAF_SITE}
+  #### Click the URL to go to the Fastly NGWAF workspace ####
+  https://manage.fastly.com/security/ngwaf/workspaces/${fastly_ngwaf_workspace.tenant_workspace.id}/dashboards
 
   tfmultiline
 
